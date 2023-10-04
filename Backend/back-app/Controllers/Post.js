77732245e1a404b15144bef18715ff94c57a978a
Oscar Anillo <oscarani14@gmail.com>
2023-10-04 18:03:49 -0500

    add picturePath to createPost controller

--- Backend/back-app/Controllers/Post.js
+++ Backend/back-app/Controllers/Post.js
@@ -14,6 +14,7 @@ export const createPost = async (req, res) => {
             location: user.location,
             description,
             userPicturePath: user.picturePath,
+            picturePath,
             likes: {},
             comments: []
         })
